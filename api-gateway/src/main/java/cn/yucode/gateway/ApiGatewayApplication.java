0cc11fd9ff5187461620829ef5d2d572e11ab800
YuGan <ganyu1016@gmail.com>
2017-07-10 00:26:04 +0800

    project init

---
+++ api-gateway/src/main/java/cn/yucode/gateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package cn.yucode.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by Gan on 2017/7/10.
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
---
+++ sc-registry/src/main/java/cn/yucode/registry/RegistryApplication.java
@@ -0,0 +1,20 @@
+package cn.yucode.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心启动类
+ * <p>
+ * Created by Gan on 2017/7/6.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+
+}
